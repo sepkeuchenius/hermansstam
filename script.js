1f04375d78ac4e723703e84eb28e03bc035c86ba
sepkeuchenius <septimber@gmail.com>
2019-01-28 12:36:57 +0100

    background

---
+++ script.js
@@ -0,0 +1,26 @@
+var $name = $('#name');
+var newC = '';
+for(var i=0; i<$name.text().length; i++){
+  var sub = $name.text().substr(i, 1);
+  if(sub != ' '){
+    newC += '<span>' + sub + '</span>'
+  }
+  else{ newC += '<span>' + '&nbsp' + '</span>'}
+}
+
+$name.html(newC);
+var $spans = $name.find('span');
+     var spanCounter = 0;
+     var spanDelay = 50;
+
+     // Loop through all spans and activate
+     $spans.each(function() {
+       var $span = $(this);
+
+       // Trigger a timeout so each span is offset
+       setTimeout(function() {
+         $span.toggleClass('active');
+       }, (spanCounter * spanDelay));
+
+       spanCounter++;
+     });
