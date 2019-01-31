63d8c206f6b4cf02f494e110ff541daadde19a69
sepkeuchenius <septimber@gmail.com>
2019-01-31 14:09:30 +0100

    update

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ var $name = $('#name');
 var $sub = $('#sub')
 var colors = ['#daf7a6', '#ffc300', '#ff5733', '#c70039', '#900c3f', '#581845','#900c3f','#c70039','#ff5733','#ffc300','#daf7a6'];
 var mcolors = ['#daf7a6', 'rgb(88, 83, 188)']
+var mborder = ['none', '1 px solid #daf7a6']
 var newC = '';
 var i =0;
 for(var i=0; i<$name.text().length; i++){
@@ -27,6 +28,7 @@ var $spans = $name.find('span');
        setTimeout(function() {
          $span.toggleClass('active');
          $span.css('color', mcolors[Math.floor(index/7)]);
+         $span.css('border-bottom', mborder[Math.floor(index/7)]);
        }, (spanCounter * spanDelay));
 
        spanCounter++;
